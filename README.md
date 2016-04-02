5e8dd2019d31ec954147c647bd5b6850a038e225
Garrett Thornburg <film42@gmail.com>
2016-04-02 11:12:27 -0600

    Add some vendor source and a mesos patch

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Mesos Lambda
 
 This is me attempting to create a proof-of-concept Amazon Lambda using docker + mesos. You can
 follow along with the build log.
+
+Download the source of mesos 0.27 and place files in `vendor/mesos-0.27.0`, apply the patch found
+in `patches/mesos-0.27.0` to allow docker to respond with a capture from stdout. Build mesos using
+their build instructions.
+
+For some reason I cannot remember right now, I needed to add `vendor/protobuf-2.5.0`, which can be
+found in the mesos-0.27.0 release. This should probably be cleaned up.
---
+++ vendor/scheduler_api/http_examples.cpp
@@ -0,0 +1,158 @@
+#include "server_http.hpp"
+#include "client_http.hpp"
+
+//Added for the json-example
+#define BOOST_SPIRIT_THREADSAFE
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+//Added for the default_resource example
+#include <fstream>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+//Added for the json-example:
+using namespace boost::property_tree;
+
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
+
+int main() {
+    //HTTP-server at port 8080 using 4 threads
+    HttpServer server(8080, 4);
+    
+    //Add resources using path-regex and method-string, and an anonymous function
+    //POST-example for the path /string, responds the posted string
+    server.resource["^/string$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        //Retrieve string:
+        auto content=request->content.string();
+        //request->content.string() is a convenience function for:
+        //stringstream ss;
+        //ss << request->content.rdbuf();
+        //string content=ss.str();
+        
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+    };
+    
+    //POST-example for the path /json, responds firstName+" "+lastName from the posted json
+    //Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
+    //Example posted json:
+    //{
+    //  "firstName": "John",
+    //  "lastName": "Smith",
+    //  "age": 25
+    //}
+    server.resource["^/json$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        try {
+            ptree pt;
+            read_json(request->content, pt);
+
+            string name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
+
+            response << "HTTP/1.1 200 OK\r\nContent-Length: " << name.length() << "\r\n\r\n" << name;
+        }
+        catch(exception& e) {
+            response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
+        }
+    };
+    
+    //GET-example for the path /info
+    //Responds with request-information
+    server.resource["^/info$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        stringstream content_stream;
+        content_stream << "<h1>Request from " << request->remote_endpoint_address << " (" << request->remote_endpoint_port << ")</h1>";
+        content_stream << request->method << " " << request->path << " HTTP/" << request->http_version << "<br>";
+        for(auto& header: request->header) {
+            content_stream << header.first << ": " << header.second << "<br>";
+        }
+        
+        //find length of content_stream (length received using content_stream.tellp())
+        content_stream.seekp(0, ios::end);
+        
+        response <<  "HTTP/1.1 200 OK\r\nContent-Length: " << content_stream.tellp() << "\r\n\r\n" << content_stream.rdbuf();
+    };
+    
+    //GET-example for the path /match/[number], responds with the matched string in path (number)
+    //For instance a request GET /match/123 will receive: 123
+    server.resource["^/match/([0-9]+)$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        string number=request->path_match[1];
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
+    };
+    
+    //Default GET-example. If no other matches, this anonymous function will be called. 
+    //Will respond with content in the web/-directory, and its subdirectories.
+    //Default file: index.html
+    //Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
+    server.default_resource["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        boost::filesystem::path web_root_path("web");
+        if(!boost::filesystem::exists(web_root_path))
+            cerr << "Could not find web root." << endl;
+        else {
+            auto path=web_root_path;
+            path+=request->path;
+            if(boost::filesystem::exists(path)) {
+                if(boost::filesystem::canonical(web_root_path)<=boost::filesystem::canonical(path)) {
+                    if(boost::filesystem::is_directory(path))
+                        path+="/index.html";
+                    if(boost::filesystem::exists(path) && boost::filesystem::is_regular_file(path)) {
+                        ifstream ifs;
+                        ifs.open(path.string(), ifstream::in | ios::binary);
+                        
+                        if(ifs) {
+                            ifs.seekg(0, ios::end);
+                            size_t length=ifs.tellg();
+                            
+                            ifs.seekg(0, ios::beg);
+                            
+                            response << "HTTP/1.1 200 OK\r\nContent-Length: " << length << "\r\n\r\n";
+                            
+                            //read and send 128 KB at a time
+                            size_t buffer_size=131072;
+                            vector<char> buffer;
+                            buffer.reserve(buffer_size);
+                            size_t read_length;
+                            try {
+                                while((read_length=ifs.read(&buffer[0], buffer_size).gcount())>0) {
+                                    response.write(&buffer[0], read_length);
+                                    response.flush();
+                                }
+                            }
+                            catch(const exception &e) {
+                                cerr << "Connection interrupted, closing file" << endl;
+                            }
+
+                            ifs.close();
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+        string content="Could not open path "+request->path;
+        response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+    };
+    
+    thread server_thread([&server](){
+        //Start server
+        server.start();
+    });
+    
+    //Wait for server to start so that the client can connect
+    this_thread::sleep_for(chrono::seconds(1));
+    
+    //Client examples
+    HttpClient client("localhost:8080");
+    auto r1=client.request("GET", "/match/123");
+    cout << r1->content.rdbuf() << endl;
+
+    string json_string="{\"firstName\": \"John\",\"lastName\": \"Smith\",\"age\": 25}";
+    auto r2=client.request("POST", "/string", json_string);
+    cout << r2->content.rdbuf() << endl;
+    
+    auto r3=client.request("POST", "/json", json_string);
+    cout << r3->content.rdbuf() << endl;
+        
+    server_thread.join();
+    
+    return 0;
+}
